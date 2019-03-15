a999394882cf3c78f32e63fbdee5282851da8290
Aleksej <zmeikov28@mail.ru>
2019-03-15 15:54:19 +0000

    UTF-8 to CP-1251

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <cstring>
+#include <iconv.h>
 
 int main() {
     int sock;
     struct sockaddr_in addr{};
     const int size = 1024;
-    char buffer[size];
+    char buffer1[size], buffer2[size];
 
     addr.sin_family = AF_INET;
     addr.sin_port = htons(2345);
@@ -22,10 +23,17 @@ int main() {
     connect(sock, (struct sockaddr *)&addr, sizeof(addr));
 
     while (true) {
-        fgets(buffer, size, stdin);
-        write(sock, buffer, size);
-        memset(buffer, 0, size);
-        read(sock, buffer, size);
-        printf("Response: %s\n", buffer);
+        fgets(buffer1, size, stdin);
+        size_t src_len = size;
+        size_t dst_len = size;
+        char *p_in = buffer1;
+        char *p_out = buffer2;
+        iconv_t conv = iconv_open("CP1251","UTF-8");
+        iconv(conv, &p_in, &src_len, &p_out, &dst_len);
+        iconv_close(conv);
+        write(sock, buffer2, size);
+        memset(buffer2, 0, size);
+        read(sock, buffer2, size);
+        printf("Response: %s\n", buffer2);
     }
 }
